6323711a0964312f5eee3d8011adce716e37477a
gthawkins24 <gthawkins24@gmail.com>
2023-02-14 15:55:41 -0500

    Lesson 30

--- Program.cs
+++ Program.cs
@@ -6,23 +6,14 @@ namespace c_sharp_masterclass
     {
         static void Main(string[] args)
         {
-            string firstName = "Teelicious";
-            string lastName = "Bohne";
-            string fullName = string.Concat("", firstName, lastName, "");
-            Console.WriteLine(fullName); // output:TeeliciousBohne
-            Console.WriteLine(firstName.Substring(2)); // output:eelicious
-            Console.WriteLine(firstName.ToLower()); // output:teelicious
-            Console.WriteLine(firstName.ToUpper()); // output:TEELICIOUS
-            Console.WriteLine(firstName.Trim()); // output:TeeliciousBohne
-            Console.WriteLine(firstName.IndexOf('e')); // output: 1
-            Console.WriteLine(String.IsNullOrWhiteSpace(firstName)); // output:false
 
-            // In C#, the String.Format method is used to insert the object or variable value inside an string.
-            // With the String.Format we can replace the value in the specified format.
-            // Syntax:
-            string newFirstName = String.Format("any string {0}", firstName);
-            Console.WriteLine(newFirstName);
-            Console.ReadLine();
+            Console.WriteLine("Please enter your name and press enter");
+
+            string input = Console.ReadLine();
+            Console.WriteLine(input.ToUpper());
+            Console.WriteLine(input.ToLower());
+            Console.WriteLine(input.Trim());
+            Console.WriteLine(input.Substring(0));
         }
     }
 }
