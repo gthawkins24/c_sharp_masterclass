b3914f164cdaa461460d482e29a096db872ac0df
gthawkins24 <gthawkins24@gmail.com>
2023-02-14 12:30:43 -0500

    Lesson 27

--- Program.cs
+++ Program.cs
@@ -6,7 +6,29 @@ namespace c_sharp_masterclass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name = "Tee Bohne";
+            int age = 9;
+
+            // String Concatenation
+            Console.WriteLine("String Concatenation");
+            Console.WriteLine("Hello, my name is " + name + " and I am " + age + " years old.");
+
+            // String Formatting
+            // string formatting uses index
+            Console.WriteLine("String Formatting");
+            Console.WriteLine("Hello, my name is {0} and I am {1} years old.", name, age);
+
+            // String Interpolation
+            // String interpolation uses $ at the start which will allow us to write out variables like this: {variableName}
+            Console.WriteLine("String Interpolation");
+            Console.WriteLine($"Hello, my name is {name} and I am {age} years old.");
+
+            // Verbatim Strings
+            // Literally and ignore any spaces and escape characters
+            Console.WriteLine("Verbatim Strings");
+            Console.WriteLine(@"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer interdum pretium libero sit amet hendrerit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut varius nibh eget orci mattis finibus. Etiam odio mauris, condimentum eget tortor non, gravida laoreet orci. Nam placerat, magna ut gravida euismod, urna est porttitor sem, id condimentum dui diam id purus. Donec ligula quam, lacinia eu nulla eu, vehicula egestas enim. Donec eu massa turpis.");
+
+            Console.ReadLine();
         }
     }
 }
