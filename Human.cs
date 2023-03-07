c00b5aa670d551198a4ee122449b00c8ed0c7fd4
gthawkins24 <gthawkins24@gmail.com>
2023-03-07 13:45:59 -0500

    Lesson 47: Our First Own Class

---
+++ Human.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_masterclass
+{
+    // this class is a blueprint for a datatype
+    internal class Human
+    {
+        // member variable    
+        public string firstName = "T-";
+        public string lastName = "Bone";
+
+        // member method
+        public void IntroduceMyself()
+        {
+            Console.WriteLine("Hi, I'm {0} {1}", firstName, lastName);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,24 +10,16 @@ namespace c_sharp_masterclass
     {
         static void Main(string[] args)
         {
-            bool isAdmin;
-            bool isRegistered = true;
-            string userName = "";
-            Console.WriteLine("Please enter your username:");
-            userName = Console.ReadLine();
+            // create an object of my class
+            // an instance of Human
+            Human denis = new Human();
+            // access public variable from outside, and even change it
+            denis.firstName = "Denis";
+            // call methods of the class
+            denis.IntroduceMyself();
 
-            if (isRegistered)
-            {
-                Console.WriteLine("Hi there, registered user");
-                if (userName != "")
-                {
-                    Console.WriteLine("Hi there, " + userName);
-                    if (userName.Equals("Admin"))
-                    {
-                        Console.WriteLine("Hi there, Admin");
-                    }
-                }
-            }
+            Human michael = new Human();
+            michael.IntroduceMyself();
         }
     }
 }
